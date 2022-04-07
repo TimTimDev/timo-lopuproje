ee7213f440640bd61f530d6a5ff9d6ef317b37d5
FluffyQuake <93243216+FluffyQuake@users.noreply.github.com>
2022-04-07 11:32:29 +0300

    git push

---
+++ src/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+    <footer>
+        <div class="relative h-[450px] w-screen p-12">
+            <div class="absolute left-28 font-serif pl-20 pt-8 text-left flex gap-y-4 flex-col">
+                <p>ABOUT US</p>
+                <p>HELP & SUPPORT</p>
+                <p>RETURNS & WARRANTY</p>
+            </div>
+            <div class="absolute right-[200px] pt-4 text-left">
+                <p class="text-[24px] font-bold">STAY UP TO DATE</p>
+                <p class="text-[16px]">Sign up and received all the latest news and offers directly to your inbox.</p>
+            </div>
+            <div class="border-2 rounded-full absolute right-[100px] h-[75px] bottom-60 w-[600px] items-center justify-left flex">
+                <p class="text-center text-bold pl-8">ENTER YOUR EMAIL</p>
+                <button class="text-center text-bold pr-8 right-0 absolute">SUBMIT</button>
+            </div>
+            <div class="absolute right-[450px] bottom-[180px] flex flex-row gap-x-4">
+                <p class="text-light text-[14px]">FOLLOW US</p>
+                <img src="/icons8-instagram.svg" alt="ig">
+                <img src="/icons8-twitter.svg" alt="tiwtter">
+                <img src="/icons8-facebook.svg" alt="facebook">
+                <img src="/icons8-youtube-squared.svg" alt="youtube">
+            </div>
+        </div>
+        <div class="flex flex-row relative mx-4">
+            <p class="">© 2009 – 2022 Chapman Guitars Ltd • All Rights Reserved</p>
+            <p>| Terms & Conditions |</p>
+            <p>Privacy Policy</p>
+            <p class="absolute right-0">Site by Thinking Juice</p>
+        </div>
+    </footer>
+</template>
