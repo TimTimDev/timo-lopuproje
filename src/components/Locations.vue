9ec037ef06a5b5058fadbfab473ca6dcc630f9a1
oliverounaid <oliverounaid@gmail.com>
2022-04-08 09:17:40 +0300

    added Locations and Maps, fixed footer width

---
+++ src/components/Locations.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import Maps from './Maps.vue'
+</script>
+
+<template>
+<div class="py-[100px]">
+  <h1 class="uppercase text-5xl">Find a dealer</h1>
+</div>
+<Maps/>
+</template>
+
+<style>
+</style>
